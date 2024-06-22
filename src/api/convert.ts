4097ad21813a8548f9c94cbc1c8fd9da1499bd14
voces <chakrawc@gmail.com>
2024-06-22 14:31:42 -0700

    fix: make flag optional on regex

--- src/api/convert.ts
+++ src/api/convert.ts
@@ -4,7 +4,7 @@ export const zAlertFromApi = zAlert.extend({
   rules: zAlert.shape.rules.transform((s) =>
     s.map((r) => {
       if (typeof r.value !== "string") return r;
-      const [, pattern, flags] = r.value.match(/^\/(.*)\/(\w+)$/) ?? [];
+      const [, pattern, flags] = r.value.match(/^\/(.*)\/(\w*)$/) ?? [];
       return {
         key: r.key,
         value: pattern ? new RegExp(pattern, flags || undefined) : r.value,
